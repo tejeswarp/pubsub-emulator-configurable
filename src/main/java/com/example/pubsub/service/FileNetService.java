7568aff64c840bb7389a470db4cf399a4c285d97
Swathi-Palakodeti <swathi.ivaturi09@gmail.com>
2025-07-02 13:09:50 +0530

    circuit breaker

--- src/main/java/com/example/pubsub/service/FileNetService.java
+++ src/main/java/com/example/pubsub/service/FileNetService.java
@@ -1,5 +1,6 @@
 package com.example.pubsub.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Service;
 
@@ -8,19 +9,36 @@ import java.util.Random;
 @Service
 public class FileNetService {
 
-    @Retry(name = "filenetService", fallbackMethod = "fallback")
+    // Outer layer with Retry
+    @Retry(name = "filenetRetry", fallbackMethod = "retryFallback")
     public String uploadDocument(String partyId, String fileName) {
-        System.out.println("Inside method uploadDocument: ");
-        if (new Random().nextBoolean()) {
-            System.out.println("Document uploaded successfully for: " + partyId + ", file: " + fileName);
-//            throw new RuntimeException("FileNet upload failed");
-            return "FN-" + partyId + "-" + fileName + "-" + System.currentTimeMillis();
-        }
-        throw new RuntimeException("FileNet upload failed");
+        return uploadWithCircuitBreaker(partyId, fileName);
     }
 
-    public String fallback(String partyId, String fileName, Exception ex) {
-        System.out.println("Retry failed for: " + partyId + ", file: " + fileName);
+    // Inner layer with CircuitBreaker
+    @CircuitBreaker(name = "filenetCB", fallbackMethod = "cbFallback")
+    public String uploadWithCircuitBreaker(String partyId, String fileName) {
+        System.out.println("Calling FileNet: " + partyId + ", " + fileName);
+
+        if (new Random().nextBoolean()) {
+            System.out.println("File uploaded for: " + partyId);
+            throw new RuntimeException("Simulated FileNet failure");
+//            return "FN-" + partyId + "-" + fileName + "-" + System.currentTimeMillis();
+        }
+
+        System.out.println("Simulated failure for: " + partyId);
+        throw new RuntimeException("Simulated FileNet failure");
+    }
+
+    // Retry fallback
+    public String retryFallback(String partyId, String fileName, Throwable ex) {
+        System.out.println("Retry fallback triggered for: " + partyId + " due to: " + ex.getMessage());
+        return null;
+    }
+
+    // Circuit breaker fallback
+    public String cbFallback(String partyId, String fileName, Throwable ex) {
+        System.out.println("⚡ CircuitBreaker fallback: Circuit open or 100% failure rate. " + ex.getMessage());
         return null;
     }
 }
